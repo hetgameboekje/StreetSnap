1a7482ea3aed2ba08da0ced28ba3107952fdeeb4
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2023-03-03 18:37:20 +0100

    Added html

--- Back_end/pdo.php
+++ Back_end/pdo.php
@@ -49,9 +49,20 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend page</title>
+</head>
+<body>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     <label for="name">Name:</label>
     <input type="text" id="name" name="name">
     <input type="submit" value="Add">
 </form>
+</body>
+</html>
+
