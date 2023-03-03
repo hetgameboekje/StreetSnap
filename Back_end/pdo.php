6a8bb548cdc392bb99d6550d19894d8d4ef4af38
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2023-03-03 18:08:11 +0100

    update

--- Back_end/pdo.php
+++ Back_end/pdo.php
@@ -1,20 +1,32 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    // Establish database connection
-    $db = new PDO("mysql:host=172.18.0.2;port=3306;dbname=test_db", "timo", "timo");
-
-    // Check if the 'tb_test' table exists, if not, create it
-    $stmt = $db->prepare("CREATE TABLE IF NOT EXISTS tb_test (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL)");
-    $stmt->execute();
-
-    // Get the name input from the form
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['name'];
-
-    // Prepare and execute an SQL statement to insert the name into the 'tb_test' table
-    $stmt = $db->prepare("INSERT INTO tb_test (name) VALUES (:name)");
-    $stmt->execute(array(':name' => $name));
+    
+    // Create a new PDO object
+    $dsn = 'mysql:host=localhost;dbname=test_db';
+    $username = 'timo';
+    $password = 'timo';
+    
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // Prepare the SQL statement
+        $stmt = $pdo->prepare("INSERT INTO tb_test (name) VALUES (:name)");
+        
+        // Bind parameters
+        $stmt->bindParam(':name', $name);
+        
+        // Execute the statement
+        $stmt->execute();
+        
+        echo "Data inserted successfully.";
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
