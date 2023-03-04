0afdcb71e7f46dec7b5dcad4a2a376a6be86ec33
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2023-03-04 10:11:34 +0100

    added password + image

--- Back_end/pdo.php
+++ Back_end/pdo.php
@@ -7,9 +7,11 @@
     <title>Backend page</title>
 </head>
 <body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" required> 
+    <input type="text" id="password" name="password" required>
+    <input type="file" name="image">
     <input type="submit" value="Add">
 </form>
 </body>
@@ -35,9 +37,15 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the name input from the form
     $name = $_POST["name"];
-    
+    $password = $_POST["password"];
+
+    // Upload file
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+
     // Insert the new name into the tb_test table
-    $sql = "INSERT INTO tb_test (name) VALUES ('$name')";
+    $sql = "INSERT INTO tb_test (name, password, image_path) VALUES ('$name', '$password', '$target_file')";
     if ($conn->query($sql) === TRUE) {
         echo "New name added successfully <br>";
         // Use PRG to redirect to the same page
@@ -56,7 +64,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // Output the data for each row
     while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . "<br>";
+        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - password = " . $row["password"] . " - image path = " . $row["image_path"] . "<br>";
     }
 } else {
     echo "No results found";
@@ -66,4 +74,3 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
-
