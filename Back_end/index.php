028bdbdc556f47985f5b05da9af6b2fe0b237f1a
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2023-03-08 09:35:19 +0100

    Connecter los gekoppelt van database

--- Back_end/index.php
+++ Back_end/index.php
@@ -21,19 +21,7 @@
 
 <?php
 
-// Set the database credentials
-$servername = "localhost:3306";
-$username = "timo";
-$password = "timo";
-$dbname = "test_db";
-
-// Create a database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include_once 'pdo.php';
 
 // Check if form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
