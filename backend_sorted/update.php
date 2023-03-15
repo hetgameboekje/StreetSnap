7241b6133e2dc87be369830eb3c54a28bf320d46
Bushman-31 <513323@vistacollege.nl>
2023-03-15 22:34:34 +0100

    update
    interface and input

--- backend_sorted/update.php
+++ backend_sorted/update.php
@@ -1,5 +1,8 @@
 <?php
 require 'pdo.php';
+
+$id=""
+
 if(isset($_GET['id'])){
     $id = $_GET['id'];
 
@@ -7,8 +10,9 @@ if(isset($_GET['id'])){
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
-$sql = "UPDATE tb_name SET 'name='$name' WHERE id=$id";
+$sql = "SELECT tb_name WHERE id=$id";
+echo $_row['name']
+//$sql = "UPDATE tb_name SET 'name='$name' WHERE id=$id";
 
 if (mysqli_query($conn, $sql)) {
   echo "Record updated successfully";
@@ -20,3 +24,23 @@ mysqli_close($conn);
 }
     
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required placeholder="First name"> 
+    <input type="text" id="password" name="password" required placeholder="Last name">
+    <input type="file" name="image" >
+    <input type="hidden" name="frmInsertUser" value="frmInsertUser" />
+    <input type="submit" value="Add">
+</form>
+</body>
+</html>
